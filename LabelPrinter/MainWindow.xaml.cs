1ca02b0c51bb3edee92d7c678e371935e9809406
shantosiddiq <shanto.siddiq1@gmail.com>
2018-08-03 20:30:42 +0600

    Some dummy code removed..

--- LabelPrinter/MainWindow.xaml.cs
+++ LabelPrinter/MainWindow.xaml.cs
@@ -15,27 +15,6 @@ using System.Windows.Shapes;
 
 namespace LabelPrinter
 {
-    public class BoolToStringConverter : BoolToValueConverter<FontWeight> { }
-    public class BoolToValueConverter<T> : IValueConverter
-    {
-        public T FalseValue { get; set; }
-        public T TrueValue { get; set; }
-
-        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
-        {
-            if (value == null)
-                return FalseValue;
-            else
-                return (bool)value ? TrueValue : FalseValue;
-        }
-
-        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
-        {
-            return value != null ? value.Equals(TrueValue) : false;
-        }
-    }
-
-    
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
