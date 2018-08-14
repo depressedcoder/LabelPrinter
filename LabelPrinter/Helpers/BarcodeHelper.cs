f814224ef682977f573694eb41991f2fe2294f97
Atish Kumar Dipongkor <atishiitdu@hotmail.com>
2018-08-14 11:00:35 +0600

    GetBarcode refactoring

--- LabelPrinter/Helpers/BarcodeHelper.cs
+++ LabelPrinter/Helpers/BarcodeHelper.cs
@@ -3,8 +3,6 @@ using iTextSharp.text.pdf;
 using System;
 using System.Drawing;
 using System.Drawing.Drawing2D;
-using System.Linq;
-using System.Text;
 using System.Text.RegularExpressions;
 
 namespace LabelPrinter.Helpers
@@ -15,69 +13,74 @@ namespace LabelPrinter.Helpers
 
         public BarcodeHelper()
         {
-            _barcode = new BarcodeLib.Barcode();
-            _barcode.Alignment = BarcodeLib.AlignmentPositions.LEFT;
+            _barcode = new BarcodeLib.Barcode
+            {
+                Alignment = BarcodeLib.AlignmentPositions.LEFT,
+                LabelPosition = BarcodeLib.LabelPositions.BOTTOMCENTER
+            };
         }
+
         public Image GetBarcode(string selectedBarcode, string barCodeLabel, int width, int height)
         {
             width = width * 100;
             height = height * 20;
-            var label = Regex.Replace(Regex.Replace(barCodeLabel, "<BAR|\\++>|>", ""),@"\D","0");
-                switch (selectedBarcode)
-                {
-                    case "Code39":
-                    _barcode.IncludeLabel = true;
-                    _barcode.LabelPosition = BarcodeLib.LabelPositions.BOTTOMCENTER;
-                    return _barcode.Encode(BarcodeLib.TYPE.CODE39, label.Length==0?"0":label, Color.Black, Color.White, width, height);
-                    case "Code128":
-                    _barcode.IncludeLabel = true;
-                    _barcode.LabelPosition = BarcodeLib.LabelPositions.BOTTOMCENTER;
-                    return _barcode.Encode(BarcodeLib.TYPE.CODE128, label.Length == 0 ? "0" : label, Color.Black, Color.White, width, height);
-                    case "EAN13":
+
+            var label = Regex.Replace(Regex.Replace(barCodeLabel, "<BAR|\\++>|>", ""), @"\D", "0");
+
+            if (string.IsNullOrEmpty(label))
+                label = "0";
+
+            switch (selectedBarcode)
+            {
+                case "Code39":
+                    return _barcode.Encode(BarcodeLib.TYPE.CODE39, label, Color.Black, Color.White, width, height);
+                case "Code128":
+                    return _barcode.Encode(BarcodeLib.TYPE.CODE128, label, Color.Black, Color.White, width, height);
+                case "EAN13":
+                    {
+                        if (label.Length < 13)
                         {
-                            if (label.Length < 13)
-                            {
-                                label = label.PadLeft(13, '0');
-                            }
-                            _barcode.IncludeLabel = true;
-                            _barcode.LabelPosition = BarcodeLib.LabelPositions.BOTTOMCENTER;
-                            return _barcode.Encode(BarcodeLib.TYPE.EAN13, label.Length == 0 ? "0" : label, Color.Black, Color.White, width, height);
+                            label = label.PadLeft(13, '0');
                         }
-                    case "EAN8":
+                        _barcode.IncludeLabel = true;
+                        return _barcode.Encode(BarcodeLib.TYPE.EAN13, label, Color.Black, Color.White, width, height);
+                    }
+                case "EAN8":
+                    {
+                        if (label.Length < 8)
                         {
-                            if (label.Length < 8)
-                            {
-                                label = label.PadLeft(8, '0');
-                            }
-                            _barcode.IncludeLabel = true;
-                            return _barcode.Encode(BarcodeLib.TYPE.EAN8, label.Length == 0 ? "0" : label, Color.Black, Color.White, width, height);
+                            label = label.PadLeft(8, '0');
                         }
-                    case "2/5 Interleaved":
+                        _barcode.IncludeLabel = true;
+                        return _barcode.Encode(BarcodeLib.TYPE.EAN8, label, Color.Black, Color.White, width, height);
+                    }
+                case "2/5 Interleaved":
+                    {
+                        if (label.Length < 4)
                         {
-                                if (label.Length < 4)
-                                {
-                                    label = label.PadLeft(4, '0');
-                                }
-                                return _barcode.Encode(BarcodeLib.TYPE.Interleaved2of5, label.Length == 0 ? "0" : label, Color.Black, Color.White, width, height);
+                            label = label.PadLeft(4, '0');
                         }
-                    case "DataMatrix":
+                        return _barcode.Encode(BarcodeLib.TYPE.Interleaved2of5, label, Color.Black, Color.White, width, height);
+                    }
+                case "DataMatrix":
+                    {
+                        BarcodeDatamatrix barcodeDatamatrix = new BarcodeDatamatrix
                         {
-                            BarcodeDatamatrix barcodeDatamatrix = new BarcodeDatamatrix
-                            {
-                                Height = 24,
-                                Width = 24,
-                                Options = BarcodeDatamatrix.DM_AUTO,
-                                ForceSquareSize = true
-                            };
-                            //https://online-barcode-reader.inliteresearch.com/
-                            barcodeDatamatrix.Generate(label);
-                            var image = barcodeDatamatrix.CreateDrawingImage(Color.Black, Color.White);
-                            var newImage = Resize(image, 48, 48, false);
-                            return newImage;
-                        }
-                    default:
-                        return null;
-                }
+                            Height = 24,
+                            Width = 24,
+                            Options = BarcodeDatamatrix.DM_AUTO,
+                            ForceSquareSize = true
+                        };
+
+                        //https://online-barcode-reader.inliteresearch.com/
+                        barcodeDatamatrix.Generate(label);
+                        var image = barcodeDatamatrix.CreateDrawingImage(Color.Black, Color.White);
+                        var newImage = Resize(image, 48, 48, false);
+                        return newImage;
+                    }
+                default:
+                    return null;
+            }
         }
 
         public void Dispose()
