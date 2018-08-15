0f9c20a4400db5d9d7bce1a77058c92be44892aa
Atish Kumar Dipongkor <atishiitdu@hotmail.com>
2018-08-15 10:27:26 +0600

    min max validation rule

--- LabelPrinter/MainWindow.xaml.cs
+++ LabelPrinter/MainWindow.xaml.cs
@@ -1,7 +1,5 @@
 ﻿using LabelPrinter.ViewModel;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Input;
 
 namespace LabelPrinter
 {
@@ -14,10 +12,10 @@ namespace LabelPrinter
         public MainWindow()
         {
             main = new MainViewModel();
-            this.DataContext = main;
+            DataContext = main;
         }
 
-        private void UpdateLabelEvent(object sender, RoutedEventArgs e)
+        void UpdateLabelEvent(object sender, RoutedEventArgs e)
         {
             var vm = DataContext as MainViewModel;
 
@@ -26,16 +24,5 @@ namespace LabelPrinter
                 vm.PreviewLabel();
             }
         }
-
-        private void TextBox_OnPreviewTextInput(object sender, TextCompositionEventArgs e)
-        {
-            e.Handled = !IsValid(((TextBox)sender).Text + e.Text);
-        }
-        public static bool IsValid(string str)
-        {
-            int i;
-            return int.TryParse(str, out i) && i >= 1 && i <= 600;
-        }
-
     }
 }
---
+++ LabelPrinter/ValidationRules/MinMaxValidationRule.cs
@@ -0,0 +1,24 @@
+﻿using System.Globalization;
+using System.Windows.Controls;
+
+namespace LabelPrinter.ValidationRules
+{
+    public class MinMaxValidationRule : ValidationRule
+    {
+        public int MinValue { get; set; }
+        public int MaxValue { get; set; }
+
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+            var message = $"Value must be between {MinValue} and {MaxValue}";
+
+            if (!int.TryParse($"{value}", out int intValue))
+                return new ValidationResult(false, "Not a valid integer");
+
+            if (intValue < MinValue || intValue > MaxValue)
+                return new ValidationResult(false, message);
+
+            return ValidationResult.ValidResult;
+        }
+    }
+}
